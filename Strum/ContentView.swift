df7b85dfa7d87150c28f807959817f7507585044
leongo <quan.ngov1@homecredit.vn>
2025-06-13 06:37:35 +0700

    first commit

---
+++ Strum/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Strum
+//
+//  Created by leongo on 13/6/25.
+//
+
+
+//
+//  ContentView.swift
+//  Strum
+//
+//  Created by leongo on 12/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Strum/StrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrumApp.swift
+//  Strum
+//
+//  Created by leongo on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct StrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
